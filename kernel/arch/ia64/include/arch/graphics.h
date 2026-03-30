a9c9ff9972c54fecd6a6022b29ce7181dd039309
guide_X@live.com <guide_X@live.com>
2026-03-29 20:06:33 -0700

    ...

--- kernel/arch/ia64/include/arch/graphics.h
+++ kernel/arch/ia64/include/arch/graphics.h
@@ -22,7 +22,7 @@
 
 #pragma once
 
-#include <cstdint>
+#include <kernel/types.h>
 
 namespace kernel {
 namespace arch {
--- kernel/arch/ia64/include/arch/pci_audio.h
+++ kernel/arch/ia64/include/arch/pci_audio.h
@@ -10,7 +10,7 @@
 
 #pragma once
 
-#include <cstdint>
+#include <kernel/types.h>
 
 namespace kernel {
 namespace arch {
